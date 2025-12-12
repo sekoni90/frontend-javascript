e3600911f038fe61a27dd71bb86ed4324da3ee66
sekoni90 <sekonioyetunji@gmail.com>
2025-12-12 15:57:48 +0000

    initial commit

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -45,7 +45,7 @@ function createEmployee(salary: number | string): Director | Teacher {
   return new Director();
 }
 
-function isDirector(employee: Director | Teacher): employee is Director {
+export function isDirector(employee: Director | Teacher): employee is Director {
   return employee instanceof Director;
 }
 
